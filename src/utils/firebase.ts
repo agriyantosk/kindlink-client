7ecceee2f887df693347522b62069455f6cd8a04
agriyantosk <agriyantosk@gmail.com>
2024-05-03 13:32:51 +0700

    chore: Adding ENV File

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -12,12 +12,12 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD_s18kJkL67XniuznlRHRYXlaz24xRNyQ",
-    authDomain: "kindlink-42347.firebaseapp.com",
-    projectId: "kindlink-42347",
-    storageBucket: "kindlink-42347.appspot.com",
-    messagingSenderId: "943140626359",
-    appId: "1:943140626359:web:9714890ef64b9254045232",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const initialize = () => {
